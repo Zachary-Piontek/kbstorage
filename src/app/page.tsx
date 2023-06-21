e4db5427a17bafb6ed3162a6ee28c4c407137eb3
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-06-21 14:11:23 -0700

    getting only data back needed to render storage info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,27 @@ const getStorageUnits = async () => {
     apiVersion: "2022-11-15",
   });
   const storageUnits = await stripe.products.list();
-  console.log(storageUnits);
+
+  const storageUnitWithPrices = await Promise.all(
+    storageUnits.data.map(async (storageUnit) => {
+      const prices = await stripe.prices.list({
+        product: storageUnit.id,
+      });
+      return {
+        id: storageUnit.id,
+        name: storageUnit.name,
+        price: prices.data[0].unit_amount,
+        image: storageUnit.images[0],
+        currency: prices.data[0].currency,
+      };
+    })
+  );
+  return storageUnitWithPrices;
 };
 
 export default async function Home() {
   const storageUnits = await getStorageUnits();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
