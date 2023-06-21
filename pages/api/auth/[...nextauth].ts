19d4179f0a58e641e8be7a5fd55ebc74beedd22a
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-06-21 10:48:26 -0700

    stripe install, updated user model and customer id connect to stripe working

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
 
 // next auth needs pages api folder for auth to work properly, I believe docs for update in next version with fix this 
 
@@ -17,4 +18,24 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
+    // this event if for creating only one per user(sign up stripe customer)
+    events: {
+        createUser: async ({ user }) => {
+            const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+                apiVersion: "2022-11-15",
+            });
+
+            if (user.name && user.email) {
+                const customer = await stripe.customers.create({
+                    email: user.email,
+                    name: user.name,
+                });
+
+                await prisma.user.update({
+                    where: { id: user.id },
+                    data: { stripeCustomerId: customer.id },
+            })
+        }
+    },
+},
 });
