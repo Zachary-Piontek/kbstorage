5e8ff196c0bba33007438fe586f7f1fd6d0e1cfd
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-06-21 13:58:54 -0700

    getting product storage data back from stripe console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
-export default function Home() {
+import Stripe from "stripe";
+
+const getStorageUnits = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2022-11-15",
+  });
+  const storageUnits = await stripe.products.list();
+  console.log(storageUnits);
+};
+
+export default async function Home() {
+  const storageUnits = await getStorageUnits();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
