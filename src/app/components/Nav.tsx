5d2193dc71b652e7feea5dcc5f9f39529a5efbd5
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-06-21 12:38:29 -0700

    styling nav, able to signIn/Out with button, data is correct

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,17 +2,18 @@
 
 import { Session } from "next-auth";
 import { signIn } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import Image from "next/image";
 
 export default function Nav({ user }: Session & { user: any }) {
   return (
-    <nav>
+    <nav className="flex justify-between">
       <h1 className="text-3xl text-center">auth user navbar</h1>
-      <ul>
+      <ul className="flex flex-col items-center">
         <li>Storages</li>
         {/* check if user is logged in */}
         {!user && (
-          <li>
+          <li className="bg-amber-300 text-black rounded-sm">
             <button onClick={() => signIn()}>sign in</button>
           </li>
         )}
@@ -20,7 +21,16 @@ export default function Nav({ user }: Session & { user: any }) {
           <>
             <li>{user.name}</li>
             <li>
-              <Image src={user?.image} alt={user.name} width={50} height={50} />
+              <Image
+                src={user?.image}
+                alt={user.name}
+                width={50}
+                height={50}
+                className="rounded-full"
+              />
+            </li>
+            <li className="flex items-center justify-center bg-amber-300 text-black rounded-sm">
+              <button onClick={() => signOut()}>sign out</button>
             </li>
           </>
         )}
