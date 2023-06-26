04ee4a5b45f238448e9463042dad1fb6d79a9b41
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-06-26 13:44:33 -0700

    started cart list display

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -5,10 +5,17 @@ import { useCartStore } from "../../../cartStore";
 
 export default function Cart() {
   const cartStore = useCartStore();
-  console.log(cartStore.isOpen);
+
   return (
-    <div>
-      <h1>Cart</h1>
+    <div
+      onClick={() => cartStore.toggleCart()}
+      className="fixed w-full h-screen left-0 top-0 bg-amber-800/20"
+    >
+      <div className="bg-white absolute top-0 right-0 w-96 h-screen">
+        <h1 className="text-2xl font-bold text-center text-black">
+          Storage list
+        </h1>
+      </div>
     </div>
   );
 }
